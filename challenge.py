cc8706d5ff63ac62edf306a0df6d8f6cba29ebb1
oramirezperera <ramirez.orlando17@gmail.com>
2021-12-15 11:02:59 -0400

    fixed a typo in division_by_5

--- challenge.py
+++ challenge.py
@@ -10,7 +10,7 @@ def run():
     print(division_by_3(18)) # The expected output is 6
 
     division_by_5 = make_division_by(5)
-    print(division_by_5(10)) #The expected output is 20
+    print(division_by_5(100)) #The expected output is 20
 
     division_by_18 = make_division_by(18)
     print(division_by_18(54)) #The expected output is 3
