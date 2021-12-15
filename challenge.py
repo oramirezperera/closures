698382902bfa63d85820e82270bbec045487c4f9
oramirezperera <ramirez.orlando17@gmail.com>
2021-12-15 11:00:17 -0400

    made changes to challenge.py created the closure makedivision_by and make_division, let's see if it works

--- challenge.py
+++ challenge.py
@@ -1,13 +1,19 @@
 def make_division_by(n):
     """This closures returns a function that returns the division of an x number by n"""
-    pass
+    def division_by(x):
+        return x/n
+    return division_by
 
+def run():
 
-division_by_3 = make_division_by(3)
-print(division_by_3(18)) # The expected output is 6
+    division_by_3 = make_division_by(3)
+    print(division_by_3(18)) # The expected output is 6
 
-division_by_5 = make_division_by(5)
-print(division_by_5(10)) #The expected output is 20
+    division_by_5 = make_division_by(5)
+    print(division_by_5(10)) #The expected output is 20
 
-division_by_18 = make_division_by(18)
-print(division_by_18(54)) #The expected output is 3
+    division_by_18 = make_division_by(18)
+    print(division_by_18(54)) #The expected output is 3
+
+if __name__ == '__main__':
+    run()
