c8208d7d08c42638a9b349704d668b33c90e0839
oramirezperera <ramirez.orlando17@gmail.com>
2021-12-17 16:43:21 -0400

    added a max now if you pass a number in the parameters of the function that's the maximum number of loop iterations

--- generators.py
+++ generators.py
@@ -1,11 +1,11 @@
 import time
 
-def fibo_gen():
+def fibo_gen(max=None):
     n1 = 0
     n2 = 1
     counter = 0
 
-    while True:
+    while not max or counter <= max:
         if counter == 0:
             counter += 1
             yield n1
@@ -20,7 +20,7 @@ def fibo_gen():
 
 
 if __name__ == '__main__':
-    fibonacci = fibo_gen()
+    fibonacci = fibo_gen(10)
     for element in fibonacci:
         print(element)
         time.sleep(1)
