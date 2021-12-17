3bae00af1034e05f5cecd015f8f64310585e041a
oramirezperera <ramirez.orlando17@gmail.com>
2021-12-17 16:11:42 -0400

    added __init__ to have a limit in the times the loop executes

--- iterators.py
+++ iterators.py
@@ -2,6 +2,9 @@ import time
 
 class FiboIter(): 
 
+    def __init__(self, max=None):
+        self.max = max
+
     def __iter__(self):
         self.n1 = 0
         self.n2 = 1
@@ -16,16 +19,19 @@ class FiboIter():
         elif self.counter == 1:
             self.counter += 1
             return self.n2
-        else:
+        elif not self.max or self.counter <= self.max:
             self.aux = self.n1 + self.n2
             #self.n1 = self.n2
             #self.n2 = self.aux
             self.n1, self.n2 = self.n2, self.aux
             self.counter += 1
             return self.aux
+        else:
+            raise StopIteration
+
 
 if __name__ == '__main__':
-    fibonacci = FiboIter()
+    fibonacci = FiboIter(10)
     for element in fibonacci:
         print(element)
         time.sleep(1)
